66eb05742f856d4948d7f63084be435adac31021
Samuel Pessoa Madeira <spmadeira@edu.unifor.br>
2020-01-21 10:30:02 -0300

    ajustes

---
+++ Assets/Scripts/Control/MapSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class MapSettings : SerializedMonoBehaviour
+{
+    public static MapSettings Instance { get; private set;}
+
+    public void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(this);
+            return;
+        }
+        
+        Instance
+    }
+
+    public Color OverrideBackgroundColor;
+    //encontros e tal
+}
